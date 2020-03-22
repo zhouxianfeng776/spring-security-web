6745ad2d6a0095ea21fa38e72cf5dfa74687208d
zhouxianfeng776 <zhou4587211@163.com>
2020-03-22 19:02:30 +0800

    增加 session redis功能

--- src/test/java/com/zhou/demoweb/DemoWebApplicationTests.java
+++ src/test/java/com/zhou/demoweb/DemoWebApplicationTests.java
@@ -3,11 +3,19 @@ package com.zhou.demoweb;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.Duration;
+
 @SpringBootTest
 class DemoWebApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	public static void main(String[] args) {
+		//D ：天 T：天和小时之间的分隔符 H ：小时  M：分钟  S：秒 每个单位都必须是数字，且时分秒顺序不能乱
+		Duration fromChar1 = Duration.parse("PT30M");
+		System.out.println(fromChar1.getSeconds());
 	}
 
 }
